39c19cae8e545948e6228bf872cc1163e7181382
kwillis1969 <43993852+kwillis1969@users.noreply.github.com>
2018-10-09 14:19:58 +0100

    Update index.js

--- src-step-0/index.js
+++ src-step-0/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Keyboard } from './components/Keyboard'
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello TECH(K)NOW 2018!</h1>
+     <Keyboard />
     </div>
   );
 }
